862023c7988e7de03bdba229b75da5f5f43c9063
thomasyoung-audet <thomasyoungaudet@gmail.com>
2020-10-19 19:28:13 -0400

    start of evaluation functionality. calculates MAp and R-Precision

---
+++ eval.py
@@ -0,0 +1,112 @@
+import search
+import re
+from porter import PorterStemmer
+
+"""
+The final part of the assignment is to evaluate the performance of the IR system you have developed. 
+You need to write a program eval. The input to this program would be query.text and qrels.text from CACM. 
+Your program should go through all the queries in query.text, for each query, get all the relevant results 
+from your system (by running search), compare the results with the actual user judgment from qrels.text, 
+and then calculate the mean average precision (MAP) and R-Precision values. 
+
+The final output will be the average MAP and R-Precision values over all queries. 
+"""
+
+
+def eval():
+    stem = False
+    stop_words = False
+    g = open("postings.txt", "r")
+    check = g.read().replace('\n', ' ')
+    if check[0] == "1":
+        stem = True
+    if check[1] == "1":
+        stop_words = True
+    g.close()
+    rels = open("qrels.text", "r")
+    rel_context = rels.read().replace('\n', ' ').split()
+    g.close()
+    rel_context = list(filter('0'.__ne__, rel_context))
+    rel_keys = rel_context[0::2]
+    relevance_dict = dict.fromkeys(rel_keys)
+    i = 1
+    while i < len(rel_context):
+        if relevance_dict[rel_context[i-1]] is None:
+            relevance_dict[rel_context[i - 1]] = []
+        relevance_dict[rel_context[i - 1]].append(rel_context[i])
+        i += 2
+
+    q = open("query.text", "r")
+    query_content = q.readlines()
+    q.close()
+
+    querylist = get_queries(query_content, stem, stop_words)
+
+    result_list = []
+    # for query in querylist:
+    #    result_list.append(search.lookup(query))
+    result_list.append(search.lookup(querylist[0]))
+
+    # calculate Mean Average Precision
+    # calculate average precision for each query, then average those
+
+
+    # calculate R-precision
+
+
+
+
+
+
+def get_avg_prec(query_results, relevant_results):
+    relevant_found = 0
+    total_found = 0
+    precision_history = []
+    for result in query_results:
+        if result in relevant_results:
+            relevant_found += 1
+        total_found += 1
+        precision_history.append(relevant_found/total_found)
+    return sum(precision_history)/len(relevant_results)
+
+
+def get_queries(lines, use_stem, use_stop_word):
+    stop_words = []
+    if use_stop_word:
+        stop_words = open("stopwords.txt", "r").read().split('\n')
+    query_text = ""
+    queries = []
+    start_scan = False
+    for line in lines:
+        if line.startswith("."):
+            start_scan = False
+        if start_scan is True:
+            words = str.split(line)
+            for word in words:
+                # words are made lower case right from the start
+                word = word.lower()
+                word = re.sub('[^A-Za-z0-9$\-]+', '', word)
+                if use_stem:
+                    word = stem(word)
+                if use_stop_word:
+                    if word not in stop_words:
+                        query_text += word + " "
+                else:
+                    if word != '':
+                        query_text += word + " "
+        if line.startswith(".W"):
+            start_scan = True
+        if line.startswith(".N"):
+            queries.append(query_text)
+            query_text = ""
+
+    return queries
+
+
+def stem(word):
+    p = PorterStemmer()
+    return p.stem(word, 0, len(word) - 1)
+
+
+if __name__ == "__main__":
+    eval()
