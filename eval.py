4188a9ca9b177882aa68bde46381584b13ad2e19
thomasyoung-audet <thomasyoungaudet@gmail.com>
2020-10-23 14:10:34 -0400

    fixes the way R-precision is calculated.

--- eval.py
+++ eval.py
@@ -74,14 +74,20 @@ def evaluate(K):
 
 def get_avg_prec(query_results, relevant_results):
     relevant_found = 0
+    r_counter = 0
     total_found = 0
     precision_history = []
+    i = 0
     for result in query_results:
+        i += 1
         if result in relevant_results:
             relevant_found += 1
+            if i < len(query_results):
+                r_counter += 1
         total_found += 1
         precision_history.append(relevant_found / total_found)
-    r_precision = relevant_found / len(query_results)
+
+    r_precision = r_counter / len(query_results)
     return [sum(precision_history) / len(relevant_results), r_precision]
 
 
